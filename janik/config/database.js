fdf04dee5f29097aaf9bdad572fbbae5eb845c7f
janikJ <71125912+janik-1@users.noreply.github.com>
2023-04-03 21:29:51 +0200

    reconfigure Database for local instance

--- janik/config/database.js
+++ janik/config/database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'db',
-  port: 3306,
+  host: 'localhost',
+  // port: 3306,
   user: 'root',
   password: 'root_password',
   database: 'BD'
