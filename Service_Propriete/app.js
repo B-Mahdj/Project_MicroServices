0ed6fd2f20e51e0d2da0dadc264d27f014b73d42
janikJ <71125912+janik-1@users.noreply.github.com>
2023-04-06 09:27:57 +0200

    AddDockerFile and env

--- Service_Propriete/app.js
+++ Service_Propriete/app.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 app.use('/api/v1/proprietes', proprieteRoutes);
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.SERVICE_PROPRIETE_DEFAULT_PORT || 3000;
 app.listen(port, () => {
   console.log(`Le serveur a démarré sur le port ${port}.`);
 });
