1256b5a4cad21f9573e45b5e4208edee53662c97
B-Mahdj <bilal.mahdjoubi95@gmail.com>
2023-04-15 16:35:51 +0200

    Change config of database to connect to DB

--- Service_Propriete/config/database.js
+++ Service_Propriete/config/database.js
@@ -1,19 +1,8 @@
 const mysql = require('mysql2/promise');
 
-// require('dotenv').config();
-
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   //: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME
-// });
-
-
 const pool = mysql.createPool({
-  host: 'localhost',
-  // port: 3306,
+  host: 'db',
+  port: 3306,
   user: 'root',
   password: 'root_password',
   database: 'BD'
